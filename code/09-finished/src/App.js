41f190e73f273886fc148980c1edfdb9b49d6c4f
SamuelCuello <samueleloycuello@gmail.com>
2022-07-11 11:14:52 -0400

    filter tasks by year

--- code/09-finished/src/App.js
+++ code/09-finished/src/App.js
@@ -1,34 +1,52 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+let savedExpenses = [...DUMMY_EXPENSES];
 
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    const filteredExpenses = [...savedExpenses].filter(
+      (expense) => expense.date.getFullYear() === +selectedYear
+    );
+    console.log(filteredExpenses);
+    setExpenses(filteredExpenses);
+  };
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((_prevExpenses) => {
+      savedExpenses = [expense, ...savedExpenses];
+
+      return savedExpenses;
+    });
   };
 
   // return React.createElement(
@@ -41,9 +59,13 @@ const App = () => {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <Expenses
+        items={expenses}
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
     </div>
   );
-}
+};
 
 export default App;
